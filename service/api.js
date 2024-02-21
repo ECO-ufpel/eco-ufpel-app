e150299b517e8af6b0ab4b4ccc71218c98808b51
Daniel Núñez <daniel.portonunez@gmail..com>
2024-02-21 14:32:23 -0300

    fix: invalid token behaviour

--- service/api.js
+++ service/api.js
@@ -1,15 +1,26 @@
 import axios from 'axios'
+import { router } from 'expo-router'
+import * as SecureStore from 'expo-secure-store'
 
 export const api = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL,
 })
 
 api.interceptors.response.use(
-  function (response) {
+  async function (response) {
     return response.data
   },
-  function (error) {
-    console.log('error', error)
+  async function (error) {
+    const isForbidden = error.response?.status === 403
+    const token = await SecureStore.getItemAsync('session')
+
+    if (isForbidden && token) {
+      await SecureStore.deleteItemAsync('session')
+      router.replace('/sign-in')
+    }
+
+    console.log('[AXIOS] Error', error)
+
     return Promise.reject(error)
   },
 )
